bb5ad746b9ef9b8cdcb0e23aca262e5742dfcd22
Daniel Katavić <dkatav2@racunarstvo.hr>
2022-07-04 02:03:02 +0200

    Minor changes

--- PublicSite/Controllers/ApartmentController.cs
+++ PublicSite/Controllers/ApartmentController.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using System.Web.Mvc;
 using DataLayer.Models;
 using DataLayer.Managers;
-using System.Linq;
 
 namespace PublicSite.Controllers
 {
     public class ApartmentController : Controller
     {
         private readonly IRepo repo = RepoFactory.GetRepo();
-        private static IList<Apartment> apartments;
-        
+
         // GET: Apartment
         public ActionResult Index() => View(repo);
 
         public ActionResult GetAllApartments(string city, string rooms, string adults, string children, string order)
         {
-            apartments = repo.LoadApartments();
+            IList<Apartment> apartments = repo.LoadApartments();
             ApartmentManager.FilterApartments(city, rooms, adults, children, ref apartments);
             ApartmentManager.OrderApartments(order, ref apartments);
             return PartialView("_ApartmentHelper", apartments);
@@ -25,7 +23,7 @@ namespace PublicSite.Controllers
 
         public ActionResult Details(int apartmentId)
         {
-            Apartment apartment = apartments.First(a => a.Id == apartmentId);
+            Apartment apartment = repo.LoadApartmentDetails(apartmentId);
             return View(apartment);
         }
     }
